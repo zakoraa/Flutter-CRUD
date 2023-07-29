90cddd4e55d1956bd24fc5a01ff5ebd893d815cc
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-29 21:02:16 +0700

    update page EditUserView

--- lib/module/editUserView/view/edit_user_view.dart
+++ lib/module/editUserView/view/edit_user_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tahap1_crud/module/changeUserDataView/view/change_user_data_view.dart';
 import 'package:tahap1_crud/module/editUserView/widgets/form_widget.dart';
 import 'package:tahap1_crud/pageTemplate/add_edit_user.dart';
 
@@ -16,19 +18,22 @@ class EditUserView extends StatelessWidget {
             leading: "Username",
             title: "NAME",
             trailing: Icons.keyboard_arrow_right,
-            onTap: () {},
+            onTap: () =>
+                Get.to(() => const ChangeUserDataView(data: "Username"),transition: Transition.rightToLeft),
           ),
           FormWidget(
             leading: "Email",
             title: "name@gmail.com",
             trailing: Icons.keyboard_arrow_right,
-            onTap: () {},
+            onTap: () =>
+                Get.to(() => const ChangeUserDataView(data: "Email"),transition: Transition.rightToLeft),
           ),
           FormWidget(
             leading: "Password",
             title: "* * * * * *",
             trailing: Icons.keyboard_arrow_right,
-            onTap: () {},
+            onTap: () =>
+                Get.to(() => const ChangeUserDataView(data: "Password"),transition: Transition.rightToLeft),
           ),
         ],
       ),
