8604a7b5f953925aecf030d95ea327796917d4f3
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-29 19:02:11 +0700

    update page AddUserView

--- lib/module/addUserView/view/add_user_view.dart
+++ lib/module/addUserView/view/add_user_view.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:tahap1_crud/pageTemplate/add_edit_user.dart';
-import 'package:tahap1_crud/widgets/button.dart';
-
 import '../../../widgets/text_field_widget.dart';
 
 class AddUserView extends StatelessWidget {
@@ -11,8 +8,9 @@ class AddUserView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AddOrEditPageTemplate(
+        titlePage: "ADD USER",
         profilePictureIcon: Icons.add,
-        textFieldForm: Column(
+        form: Column(
           children: const [
             TextFieldWidget(hintText: "Username", prefixIcon: Icons.person),
             TextFieldWidget(hintText: "Email", prefixIcon: Icons.email),
