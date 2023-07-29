e59b1073fcf63f78cdfdb982336b614781314777
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-29 23:09:57 +0700

    minor update

--- lib/service/user_service.dart
+++ lib/service/user_service.dart
@@ -1,13 +1,11 @@
 // ignore_for_file: avoid_print
 
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:tahap1_crud/model/user_model.dart';
+import 'package:uuid/uuid.dart';
 
 class UserService {
-  User? user;
-
   Future getUsers() async {
     try {
       Uri url = Uri.parse("https://capekngoding.com/6282197504499/api/users");
@@ -15,13 +13,14 @@ class UserService {
 
       Map data = jsonDecode(response.body);
       List userData = [];
-
       for (var i in data["data"]) {
         userData.add(i);
       }
       if (userData.isNotEmpty) {
         print("Get users success");
       }
+
+      print(userData);
 
       return User.usersFromSnapshot(userData);
     } catch (e) {
@@ -30,26 +29,23 @@ class UserService {
   }
 
   Future addUser(
-    String id,
     String username,
     String email,
     String password,
     String? profilePicture,
   ) async {
-    try{
-    Uri url =
-        Uri.parse("POST https://capekngoding.com/6282197504499/api/users");
-    final response = await http.post(url, body: {
-      "id": id,
-      "username": username,
-      "email": email,
-      "password": password,
-      "profile_picture": profilePicture ?? "",
-    });
+    try {
+      Uri url = Uri.parse("https://capekngoding.com/6282197504499/api/users");
+      final response = await http.post(url, body: {
+        "id": const Uuid().v4(),
+        "username": username,
+        "email": email,
+        "password": password,
+        "profile_picture": profilePicture ?? "",
+      });
 
-    var data = jsonDecode(response.body);
-    return data;
-    
+      var data = jsonDecode(response.body);
+      return data;
     } catch (e) {
       print(e.toString());
     }
@@ -62,31 +58,30 @@ class UserService {
     String password,
     String? profilePicture,
   ) async {
-    try{
-      Uri url = Uri.parse("https://capekngoding.com/6282197504499/api/users/$id");
-    final response = await http.put(url, body: {
-      "username": username,
-      "email": email,
-      "password": password,
-      "profile_picture": profilePicture ?? "",
-    });
-    var data = jsonDecode(response.body);
-    return data;
-
+    try {
+      Uri url =
+          Uri.parse("https://capekngoding.com/6282197504499/api/users/$id");
+      final response = await http.put(url, body: {
+        "username": username,
+        "email": email,
+        "password": password,
+        "profile_picture": profilePicture ?? "",
+      });
+      var data = jsonDecode(response.body);
+      return data;
     } catch (e) {
       print(e.toString());
     }
   }
 
   Future deleteUser(String id) async {
-    try{
-    Uri url =
-        Uri.parse("https://capekngoding.com/6282197504499/api/users/$id");
-    final response = await http.delete(url);
+    try {
+      Uri url =
+          Uri.parse("https://capekngoding.com/6282197504499/api/users/$id");
+      final response = await http.delete(url);
 
-    var data = jsonDecode(response.body);
-    return data;
-
+      var data = jsonDecode(response.body);
+      return data;
     } catch (e) {
       print(e.toString());
     }
