e27515dba4812fcac5f39f121f2fdb830000351a
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-29 19:04:00 +0700

    update template for AddUserView & EditUserView

--- lib/pageTemplate/add_edit_user.dart
+++ lib/pageTemplate/add_edit_user.dart
@@ -1,17 +1,21 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:tahap1_crud/widgets/button.dart';
 
 class AddOrEditPageTemplate extends StatelessWidget {
   const AddOrEditPageTemplate(
-      {required this.profilePictureIcon,
+      {super.key,
+      required this.titlePage,
+      required this.profilePictureIcon,
       this.onPressedProfilePictureIcon,
-      required this.textFieldForm,
-      required this.buttonText,
+      required this.form,
+      this.buttonText,
       this.onTapButton});
 
-  final String buttonText;
-  final textFieldForm;
+  final String? buttonText,titlePage;
+  final form;
   final profilePictureIcon;
   final onPressedProfilePictureIcon;
   final onTapButton;
@@ -36,19 +40,36 @@ class AddOrEditPageTemplate extends StatelessWidget {
                       child: Column(
                     children: [
                       Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: GestureDetector(
-                              onTap: () => Get.back(),
-                              child: const Icon(
-                                Icons.arrow_back,
-                                size: 40,
-                              )),
+                        padding: const EdgeInsets.only(left: 20, top: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Align(
+                              alignment: Alignment.centerLeft,
+                              child: GestureDetector(
+                                  onTap: () => Get.back(),
+                                  child: const Icon(
+                                    Icons.arrow_back,
+                                    size: 40,
+                                  )),
+                            ),
+                             Text(
+                              titlePage!,
+                              style: const TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 60.0,
+                            ),
+                          ],
                         ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
+                        padding:
+                            const EdgeInsets.only(top: 20, right: 20, left: 20),
                         child: Column(
                           children: [
                             Stack(
@@ -86,19 +107,21 @@ class AddOrEditPageTemplate extends StatelessWidget {
                             const SizedBox(
                               height: 70.0,
                             ),
-                            textFieldForm,
+                            form,
                             const SizedBox(
                               height: 20.0,
                             ),
-                            ButtonWidget(
-                                text: buttonText,
-                                height: 50,
-                                width: Get.size.width * 0.7,
-                                onTap: onTapButton,
-                                borderRadius: 10,
-                                fontSize: 18,
-                                bgColor:
-                                    const Color.fromARGB(255, 80, 165, 255))
+                            buttonText == null
+                                ? const SizedBox.shrink()
+                                : ButtonWidget(
+                                    text: buttonText!,
+                                    height: 50,
+                                    width: Get.size.width * 0.7,
+                                    onTap: onTapButton,
+                                    borderRadius: 10,
+                                    fontSize: 18,
+                                    bgColor:
+                                        const Color.fromARGB(255, 80, 165, 255))
                           ],
                         ),
                       ),
