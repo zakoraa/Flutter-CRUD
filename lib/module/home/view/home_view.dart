b274865d380e1fafd0d4ea9f2f8770141a778cae
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-29 23:08:43 +0700

    get data from api

--- lib/module/home/view/home_view.dart
+++ lib/module/home/view/home_view.dart
@@ -3,11 +3,14 @@ import 'package:get/get.dart';
 import 'package:tahap1_crud/module/addUserView/view/add_user_view.dart';
 import 'package:tahap1_crud/module/home/widgets/list_tile_widget.dart';
 
+import '../controller/home_controller.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    HomeController controller = Get.put(HomeController());
     return Scaffold(
         body: SingleChildScrollView(
       child: Center(
@@ -45,11 +48,19 @@ class HomeView extends StatelessWidget {
                   const SizedBox(
                     height: 50.0,
                   ),
-                  Expanded(
-                    child: Column(
-                      children: const [ListTileWiget()],
-                    ),
-                  )
+                  Obx(() => (controller.isLoading.value
+                      ? const Text(
+                          "Loading...",
+                          style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.grey,
+                          ),
+                        )
+                      : Column(
+                          children: controller.userDataList
+                              .map((user) => ListTileWidget(user: user))
+                              .toList(),
+                        )))
                 ],
               ),
             ),
