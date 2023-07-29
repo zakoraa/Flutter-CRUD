9ad98740124f2c9ed111a381f751e612989b871b
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-29 22:20:25 +0700

    add user model

---
+++ lib/model/user_model.dart
@@ -0,0 +1,24 @@
+class User {
+  String? id, username, email, password, profilePicture;
+
+  User({
+    required this.id,
+    required this.username,
+    required this.email,
+    required this.password,
+    this.profilePicture,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+        id: json['id'],
+        username: json['username'],
+        email: json['email'],
+        password: json['password'],
+        profilePicture: json['profile_picture'] ?? "");
+  }
+
+  static List<User> usersFromSnapshot(List snapshot) {
+    return snapshot.map((data) => User.fromJson(data)).toList();
+  }
+}
