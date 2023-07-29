dbfc4e0220a270ac0b62a0313a7692fdc6ddbfaf
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-30 01:06:55 +0700

    add function when delete user data success

--- lib/module/home/controller/home_controller.dart
+++ lib/module/home/controller/home_controller.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:tahap1_crud/model/user_model.dart';
 import 'package:tahap1_crud/service/user_service.dart';
+import 'package:tahap1_crud/utils/scaffold_messenger_utils.dart';
 
 class HomeController extends GetxController {
   final UserService userService = UserService();
@@ -20,5 +22,10 @@ class HomeController extends GetxController {
       userDataList.value = userData;
     }
     isLoading.value = false;
+  }
+
+  void deleteUserSuccess(BuildContext context) async {
+    ScaffoldMessengerUtils.showFloatingSnackBar(
+        context, "Delete User Success", const Color.fromARGB(255, 68, 212, 24));
   }
 }
