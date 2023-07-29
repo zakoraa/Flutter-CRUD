60bf46e283d901b5bf7871f35f7f42dd8524d7f4
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-29 21:01:54 +0700

    update page AddUserView

--- lib/module/addUserView/view/add_user_view.dart
+++ lib/module/addUserView/view/add_user_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tahap1_crud/pageTemplate/add_edit_user.dart';
-import '../../../widgets/text_field_widget.dart';
+import '../../../widgets/underline_text_field_widget.dart';
 
 class AddUserView extends StatelessWidget {
   const AddUserView({super.key});
@@ -12,10 +12,10 @@ class AddUserView extends StatelessWidget {
         profilePictureIcon: Icons.add,
         form: Column(
           children: const [
-            TextFieldWidget(hintText: "Username", prefixIcon: Icons.person),
-            TextFieldWidget(hintText: "Email", prefixIcon: Icons.email),
-            TextFieldWidget(hintText: "Password", prefixIcon: Icons.key),
-            TextFieldWidget(
+            UnderlineTextFieldWidget(hintText: "Username", prefixIcon: Icons.person),
+            UnderlineTextFieldWidget(hintText: "Email", prefixIcon: Icons.email),
+            UnderlineTextFieldWidget(hintText: "Password", prefixIcon: Icons.key),
+            UnderlineTextFieldWidget(
                 hintText: "Confirm Password", prefixIcon: Icons.key),
           ],
         ),
