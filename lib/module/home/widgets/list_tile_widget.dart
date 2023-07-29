ce090b06c0629534a1542f998c76faa88eafcc8e
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-29 15:16:45 +0700

    update home view

---
+++ lib/module/home/widgets/list_tile_widget.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../utils/button.dart';
+
+class ListTileWiget extends StatelessWidget {
+  const ListTileWiget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: Get.size.width,
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: const Color.fromARGB(139, 158, 158, 158),
+            borderRadius: BorderRadius.circular(10)),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                ClipRRect(
+                    borderRadius: BorderRadius.circular(70),
+                    child: Image.network(
+                      'https://icon-library.com/images/default-profile-icon/default-profile-icon-6.jpg',
+                      height: 70,
+                      width: 70,
+                    )),
+                const SizedBox(
+                  width: 10.0,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "NAME",
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    SizedBox(
+                      height: 2.0,
+                    ),
+                    Text(
+                      "18 years old",
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Column(
+              children: [
+                ButtonUtils(
+                    onTap: () {},
+                    text: "EDIT",
+                    height: 30,
+                    width: 100,
+                    borderRadius: 5,
+                    bgColor: const Color.fromARGB(255, 80, 165, 255)),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                ButtonUtils(
+                    onTap: () {},
+                    text: "DELETE",
+                    height: 30,
+                    width: 100,
+                    borderRadius: 5,
+                    bgColor: const Color.fromARGB(255, 255, 92, 80)),
+              ],
+            )
+          ],
+        ));
+  }
+}
