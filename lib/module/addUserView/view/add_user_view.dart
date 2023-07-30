60771582929d360cc829def6b4e7920b43276dfa
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-30 18:14:49 +0700

    add controller

--- lib/module/addUserView/view/add_user_view.dart
+++ lib/module/addUserView/view/add_user_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tahap1_crud/module/addUserView/controller/add_user_controller.dart';
 import 'package:tahap1_crud/pageTemplate/add_edit_user.dart';
 import '../../../widgets/underline_text_field_widget.dart';
 
@@ -7,21 +9,32 @@ class AddUserView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AddUserController controller = Get.put(AddUserController());
     return AddOrEditPageTemplate(
-        titlePage: "ADD USER",
-        profilePictureIcon: Icons.add,
-        form: Column(
-          children: const [
-            UnderlineTextFieldWidget(
-                hintText: "Username", prefixIcon: Icons.person),
-            UnderlineTextFieldWidget(
-                hintText: "Email", prefixIcon: Icons.email),
-            UnderlineTextFieldWidget(
-                hintText: "Password", prefixIcon: Icons.key),
-            UnderlineTextFieldWidget(
-                hintText: "Confirm Password", prefixIcon: Icons.key),
-          ],
-        ),
-        buttonText: "Add User");
+      titlePage: "ADD USER",
+      profilePictureIcon: Icons.add,
+      form: Column(
+        children: [
+          UnderlineTextFieldWidget(
+              controller: controller.username,
+              hintText: "Username",
+              prefixIcon: Icons.person),
+          UnderlineTextFieldWidget(
+              controller: controller.email,
+              hintText: "Email",
+              prefixIcon: Icons.email),
+          UnderlineTextFieldWidget(
+              controller: controller.password,
+              hintText: "Password",
+              prefixIcon: Icons.key),
+          UnderlineTextFieldWidget(
+              controller: controller.confirmPassword,
+              hintText: "Confirm Password",
+              prefixIcon: Icons.key),
+        ],
+      ),
+      buttonText: "Add User",
+      onTapButton: () => controller.checkTextField(context),
+    );
   }
 }
