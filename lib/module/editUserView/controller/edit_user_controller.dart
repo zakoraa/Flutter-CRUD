b5ba5c5e7b0a0a484e54aa7b081e34efacde5b10
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-30 19:15:29 +0700

    add controller EditUserController

---
+++ lib/module/editUserView/controller/edit_user_controller.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_typing_uninitialized_variables
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../service/user_service.dart';
+import '../../../utils/scaffold_messenger_utils.dart';
+import '../../home/controller/home_controller.dart';
+
+class EditUserController extends GetxController {
+  EditUserController({required this.user});
+
+  final user;
+
+  @override
+  void onInit() {
+    super.onInit();
+    username.text = user.username;
+    email.text = user.email;
+    password.text = user.password;
+  }
+
+  TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+
+  UserService userService = Get.put(UserService());
+  HomeController homeController = Get.put(HomeController());
+
+  RxBool isLoading = true.obs;
+
+  void checkTextField(BuildContext context) {
+    if (username.text.isEmpty || email.text.isEmpty || password.text.isEmpty) {
+      ScaffoldMessengerUtils.showFloatingSnackBar(
+          context,
+          "There is still something that hasn't been filled. Please fill it!",
+          const Color.fromARGB(255, 255, 92, 80));
+    } else {
+      isLoading.value = false;
+      userService
+          .updateUser(user.id,username.text, email.text, password.text, null)
+          .then((value) {
+        ScaffoldMessengerUtils.showFloatingSnackBar(context,
+            "Update User Success", const Color.fromARGB(255, 80, 165, 255));
+        homeController.onRefresh();
+        isLoading.value = true;
+      });
+    }
+  }
+}
