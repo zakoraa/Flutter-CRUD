c06ee312420656ba77ce19e90b4b4f3ed7b1e67f
Muhammad Rafli Silehu <raflisilehu20@gmail.com>
2023-07-30 14:47:40 +0700

    add obx & LiquidPullToRefresh

--- lib/module/home/view/home_view.dart
+++ lib/module/home/view/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 import 'package:tahap1_crud/module/home/widgets/list_tile_widget.dart';
 
 import '../../addUserView/view/add_user_view.dart';
@@ -12,61 +13,72 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     HomeController controller = Get.put(HomeController());
     return Scaffold(
-        body: SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Center(
-        child: SizedBox(
-          width: Get.size.width * 0.9,
-          child: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const SizedBox(
-                        width: 15.0,
-                      ),
-                      const Text(
-                        "Simple CRUD App",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+      body: Obx(
+        () => LiquidPullToRefresh(
+          onRefresh: controller.onRefresh,
+          backgroundColor: Colors.white,
+          showChildOpacityTransition: false,
+          springAnimationDurationInMilliseconds: 500,
+          color: Colors.grey,
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            child: Center(
+              child: SizedBox(
+                width: Get.size.width * 0.9,
+                height: Get.size.height + 100,
+                child: SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 30),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const SizedBox(
+                              width: 15.0,
+                            ),
+                            const Text(
+                              "Simple CRUD App",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () => Get.to(() => const AddUserView(),
+                                  transition: Transition.rightToLeft),
+                              child: const Icon(
+                                (Icons.person_add_alt_1),
+                                size: 30,
+                              ),
+                            )
+                          ],
                         ),
-                      ),
-                      GestureDetector(
-                        onTap: () => Get.to(() => const AddUserView(),
-                            transition: Transition.rightToLeft),
-                        child: const Icon(
-                          (Icons.person_add_alt_1),
-                          size: 30,
+                        const SizedBox(
+                          height: 50.0,
                         ),
-                      )
-                    ],
+                        controller.isLoading.value
+                            ? const Text(
+                                "Loading...",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.grey,
+                                ),
+                              )
+                            : Column(
+                                children: controller.userDataList
+                                    .map((user) => ListTileWidget(user: user))
+                                    .toList(),
+                              )
+                      ],
+                    ),
                   ),
-                  const SizedBox(
-                    height: 50.0,
-                  ),
-                  Obx(() => (controller.isLoading.value
-                      ? const Text(
-                          "Loading...",
-                          style: TextStyle(
-                            fontSize: 20,
-                            color: Colors.grey,
-                          ),
-                        )
-                      : Column(
-                          children: controller.userDataList
-                              .map((user) => ListTileWidget(user: user))
-                              .toList(),
-                        )))
-                ],
+                ),
               ),
             ),
           ),
         ),
       ),
-    ));
+    );
   }
 }
